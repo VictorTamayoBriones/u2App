af91de40152a933f55f8b3e3912fe0fa7ee11027
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-13 09:18:11 -0500

    modal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UtubeService } from '../../services/utube.service';
 import { video } from '../../models/youtube.models';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
@@ -18,6 +19,28 @@ export class HomeComponent implements OnInit {
 
           console.log(this.videos)
         })
+  }
+
+  mostrarVideo(video: video){
+    console.log(video);
+
+    Swal.fire({
+      html:`
+        <h4> ${ video.title } </h4>
+        <iframe width="100%" 
+            height="315" 
+            src="https://www.youtube.com/embed/${ video.resourceId.videoId }" 
+            frameborder="0" 
+            allow="accelerometer; 
+            autoplay; 
+            clipboard-write; 
+            encrypted-media; 
+            gyroscope; 
+            picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+      `
+    })
   }
 
 }
